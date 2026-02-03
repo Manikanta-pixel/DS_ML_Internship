70f37af8466b1605c7504d773cdd093104c1c937
Manikanta-pixel <manikantathaneeru525@gmail.com>
2026-02-03 13:25:48 +0530

    day2 task

---
+++ src/day2_python_fundamentals.py
@@ -0,0 +1,20 @@
+#age in 2030
+
+name = str(input("Enter your name:"))
+Current_Age = int(input("Enter your age:"))
+age_in_2030=Current_Age + 4
+print(f"{name} will be {age_in_2030}years old in 2030")                  
+
+#bill splitter
+
+bill = float(input("enter your bill:"))
+person = int(input("number of people:"))
+countri = bill/person
+print(f" each person countribution{countri}")
+
+#raw data 
+item_name= ("Laptop")
+quantity =(2)
+price= (499.99)
+in_stock= (True)
+print(f"item{item_name},quantity{quantity},price{price},instock{in_stock}")
